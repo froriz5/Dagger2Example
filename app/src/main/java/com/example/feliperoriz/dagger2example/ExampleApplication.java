512d0db468b15d6656c535b5b3b8f5d7447ab29c
Felipe Roriz <froriz50@gmail.com>
2017-10-08 17:36:45 -0500

    Created custom application class

---
+++ app/src/main/java/com/example/feliperoriz/dagger2example/ExampleApplication.java
@@ -0,0 +1,17 @@
+package com.example.feliperoriz.dagger2example;
+
+import android.app.Application;
+
+/**
+ * Created by feliperoriz on 10/8/17.
+ */
+
+public class ExampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
